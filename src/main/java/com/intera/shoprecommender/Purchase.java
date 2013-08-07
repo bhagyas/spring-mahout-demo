57c73954a6ff3229381535f415fe6f43ce7e876e
Bhagya Nirmaan Silva <bhagyasilva124@gmail.com>
2013-08-07 05:58:10 +0530

    added entities

--- src/main/java/com/intera/shoprecommender/Purchase.java
+++ src/main/java/com/intera/shoprecommender/Purchase.java
@@ -1,5 +1,10 @@
 package com.intera.shoprecommender;
 
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
@@ -8,4 +13,10 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooToString
 @RooJpaActiveRecord
 public class Purchase {
+
+    @ManyToOne
+    private Shopper purchaser;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    private Set<Item> items = new HashSet<Item>();
 }
