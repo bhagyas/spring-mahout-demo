a62b2214fb4953e0e15ed6f258665048a1ec9241
Bhagya Nirmaan Silva <bhagyasilva124@gmail.com>
2013-08-07 06:30:39 +0530

    added ability to show recommended items

--- src/main/java/com/intera/shoprecommender/Shopper.java
+++ src/main/java/com/intera/shoprecommender/Shopper.java
@@ -1,9 +1,16 @@
 package com.intera.shoprecommender;
 
+import java.util.List;
+
+import javax.persistence.Transient;
+
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
+
+import com.intera.shoprecommender.service.ItemRecommenderService;
+import com.intera.shoprecommender.service.ItemRecommenderServiceImpl;
 
 @RooJavaBean
 @RooToString
@@ -12,4 +19,15 @@ public class Shopper {
 	
 	@NotEmpty
 	String username;
+
+	/**
+	 * Returns a list of recommended items for a shopper
+	 * @return
+	 */
+	public List<Item> getRecommendedItems(){
+		ItemRecommenderService recommender = new ItemRecommenderServiceImpl();
+		
+		return recommender.getRecommendedItemsForShopper(this);
+	}
+
 }
--- src/main/java/com/intera/shoprecommender/service/ItemRecommenderServiceImpl.java
+++ src/main/java/com/intera/shoprecommender/service/ItemRecommenderServiceImpl.java
@@ -2,6 +2,9 @@ package com.intera.shoprecommender.service;
 
 import java.util.List;
 
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.model.DataModel;
+
 import com.intera.shoprecommender.Item;
 import com.intera.shoprecommender.Shopper;
 
@@ -9,6 +12,9 @@ public class ItemRecommenderServiceImpl implements ItemRecommenderService {
 
 	@Override
 	public List<Item> getRecommendedItemsForShopper(Shopper shopper) {
+
+		DataModel model = new FileDataModel(dataFile);
+
 		// TODO Auto-generated method stub
 		return null;
 	}
